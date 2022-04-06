0c9215f4c98896a66e5ff3667318b6dc0f2ae8dd
chedim <>
2022-04-06 15:56:20 -0400

    openapi

--- src/main/java/trycb/ExampleApplication.java
+++ src/main/java/trycb/ExampleApplication.java
@@ -2,6 +2,8 @@ package trycb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @SpringBootApplication
 public class ExampleApplication {
@@ -10,4 +12,8 @@ public class ExampleApplication {
 		SpringApplication.run(ExampleApplication.class, args);
 	}
 
+  @Bean
+  ForwardedHeaderFilter forwardedHeaderFilter() {
+     return new ForwardedHeaderFilter();
+  }
 }
